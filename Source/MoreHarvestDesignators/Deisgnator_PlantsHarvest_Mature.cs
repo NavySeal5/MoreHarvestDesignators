1e88f32b638b3d9e7257d87ff604cf185560926d
NavySeal5 <tobi1.w@gmail.com>
2020-02-25 22:19:09 +0100

    Updated to version 1.1 also finally rename the source folder

--- Source/MoreHarvestDesignators/Deisgnator_PlantsHarvest_Mature.cs
+++ Source/MoreHarvestDesignators/Deisgnator_PlantsHarvest_Mature.cs
@@ -1,25 +1,13 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Tobias
- * Date: 16.08.2018
- * Time: 18:56
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using Verse;
 using RimWorld;
 using UnityEngine;
 
-namespace MoreHarvestDesignators
-{
-	/// <summary>
-	/// Description of Designator_PlantsHarvest_Mature.
-	/// </summary>
-	public class Designator_PlantsHarvest_Mature : Designator_Plants
-	{
-		public Designator_PlantsHarvest_Mature()
-		{
+namespace MoreHarvestDesignators{
+
+	public class Designator_PlantsHarvest_Mature : Designator_Plants{
+
+		public Designator_PlantsHarvest_Mature(){
 			this.defaultLabel = "Designator_PlantsHarvest_Mature_label".Translate();
 			this.defaultDesc = "Designator_PlantsHarvest_Mature_desc".Translate();
 			this.icon = ContentFinder<Texture2D>.Get("UI/Designators/Harvest", true);
@@ -31,31 +19,23 @@ namespace MoreHarvestDesignators
 			this.designationDef = DesignationDefOf.HarvestPlant;
 		}
 
-		public override AcceptanceReport CanDesignateThing(Thing t)
-		{
+		public override AcceptanceReport CanDesignateThing(Thing t){
 			AcceptanceReport acceptanceReport = base.CanDesignateThing(t);
 			AcceptanceReport result;
-			if (!acceptanceReport.Accepted)
-			{
+			if (!acceptanceReport.Accepted){
 				result = acceptanceReport;
-			}
-			else
-			{
+			}else{
 				Plant plant = (Plant)t;
-				if (!plant.HarvestableNow || plant.def.plant.harvestTag != "Standard" || plant.Growth < 0.999f)
-				{
+				if (!plant.HarvestableNow || plant.def.plant.harvestTag != "Standard" || plant.Growth < 0.999f){
 					result = "Designator_PlantsHarvest_Mature_rejected".Translate();
-				}
-				else
-				{
+				}else{
 					result = true;
 				}
 			}
 			return result;
 		}
 
-		protected override bool RemoveAllDesignationsAffects(LocalTargetInfo target)
-		{
+		protected override bool RemoveAllDesignationsAffects(LocalTargetInfo target){
 			return target.Thing.def.plant.harvestTag == "Standard";
 		}
 	}
